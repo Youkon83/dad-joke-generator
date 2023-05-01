8a90b4cd722f5fb538281c5e448e6b028d3d7b6a
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2023-05-01 14:20:20 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const joke = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+generateJoke()
+
+function generateJoke(){
+    fetch('https://icanhazdadjoke.com', {
+        headers:{
+            'Accept': 'application/json'
+        }
+    }).then((response) => response.json())
+    .then((data) => {
+        joke.innerHTML = data.joke
+    })
+}
